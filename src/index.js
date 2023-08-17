ca39a085d2f08df02859efb43d9a142fd7058f8e
{alexius <{alexiussavage0@gmail.com}>
2023-08-17 15:33:18 -0700

    let's do this one last time2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="porttwo">
+    <BrowserRouter basename="/porttwo">
       <App />
     </BrowserRouter>
   </React.StrictMode>
